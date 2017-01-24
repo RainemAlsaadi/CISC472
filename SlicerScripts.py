7b98b477ac7b0b3f91c99057cfb1b55a9156c8ab
RaniemAlsaadi <0ra5@queensu.ca>
2017-01-23 22:01:41 -0500

    Update SlicerScripts.py

--- SlicerScripts.py
+++ SlicerScripts.py
@@ -16,33 +16,24 @@ button.show()
 ###########################################################################
 
 # Create coordinate models using the CreateModels module
-
 createModelsLogic = slicer.modules.createmodels.logic()
 ModelNode = createModelsLogic.CreateCoordinate(20,2)
 ModelNode.SetName(’NodeT’)
 
-
 # Change the color of models
-
 ModelNode.GetDisplayNode().SetColor(1,0,0)
 
-
 # Create transform nodes
-
 ModelToRas = slicer.vtkMRMLLinearTransformNode()
 ModelToRas.SetName('ModelToRas')
 slicer.mrmlScene.AddNode(ModelToRas)
 
-
 # Set transform of the transform node
-
 ModelToRasTransform = vtk.vtkTransform()
 ModelToRasTransform.PreMultiply() # This is default, but it makes explicit
 ModelToRasTransform.Translate(0, 100, 0)
 ModelToRasTransform.Update()
 ModelToRas.SetAndObserveTransformToParent(ModelToRasTransform)
 
-
 # Transform the models with transform nodes
-
 ModelNode.SetAndObserveTransformNodeID(ModelToRas.GetID())
